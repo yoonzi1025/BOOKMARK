b2bf9ff565bebf0bbfad00b84278ea450249a883
yoonzi1025 <yoonzi1025@gmail.com>
2026-04-08 21:21:29 +0900

    fix vercel routing

--- src/firebase.js
+++ src/firebase.js
@@ -2,15 +2,17 @@ import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAj9iuv85CQ6T_cEPrp4pNn9RJTCybt5cM",
-  authDomain: "bookmark-85b7b.firebaseapp.com",
-  projectId: "bookmark-85b7b",
-  storageBucket: "bookmark-85b7b.firebasestorage.app",
-  messagingSenderId: "585576007030",
-  appId: "1:585576007030:web:6c6b870ebbb50d19342657",
-  measurementId: "G-86Y0B5YTS5",
+  // Vercel에 등록한 환경 변수를 가져다 쓰도록 설정
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
 };
 
+// 파이어베이스 초기화
 const app = initializeApp(firebaseConfig);
 
+// 인증 객체 내보내기
 export const auth = getAuth(app);
